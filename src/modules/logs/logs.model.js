30aee427f6a466cb339c2c9523341ed99c3fb7a8
hello-world-ttj <tijotjoseph003@gmail.com>
2025-06-04 17:35:30 +0530

    #log_activity

--- src/modules/logs/logs.model.js
+++ src/modules/logs/logs.model.js
@@ -10,9 +10,15 @@ const logs_schema = mongoose.Schema(
     user_agent: { type: String },
     request_body: { type: Object },
     action: { type: String },
+    expiresAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    },
   },
   { timestamps: true }
 );
+
+logs_schema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 const Logs = mongoose.model("Logs", logs_schema);
 
--- src/modules/promotions/promotions.routes.js
+++ src/modules/promotions/promotions.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const promotions_controller = require("./promotions.controller");
 const check_access = require("../../middlewares/check_access");
+const log_activity = require("../../middlewares/log_activity");
 const router = express.Router();
 
 router
   .route("/")
   .get(
     check_access("promotions_management_view"),
+    log_activity("view_promotions"),
     promotions_controller.get_promotions
   )
   .post(
     check_access("promotions_management_modify"),
+    log_activity("create_promotions"),
     promotions_controller.create_promotions
   );
 
@@ -20,14 +23,17 @@ router
   .route("/:id")
   .get(
     check_access("promotions_management_view"),
+    log_activity("view_promotion"),
     promotions_controller.get_promotions_by_id
   )
   .put(
     check_access("promotions_management_modify"),
+    log_activity("update_promotion"),
     promotions_controller.update_promotions
   )
   .delete(
     check_access("promotions_management_modify"),
+    log_activity("delete_promotion"),
     promotions_controller.delete_promotions
   );
 
