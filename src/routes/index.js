e906e60271acacfcbcae57bb2fbb12b35f53ef29
hello-world-ttj <tijotjoseph003@gmail.com>
2025-06-04 14:55:43 +0530

    #notifications

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const news_route = require("../modules/news/news.routes");
 const promotions_route = require("../modules/promotions/promotions.routes");
 const events_route = require("../modules/events/events.routes");
+const notifications_route = require("../modules/notifications/notifications.routes");
 
 router.use("/news", news_route);
 router.use("/promotions", promotions_route);
 router.use("/events", events_route);
+router.use("/notifications", notifications_route);
 
 module.exports = router;
